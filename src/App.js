c22d78e59052c186241a76fa9a5dfc0414b5f873
dsab123 <dsabbaghumd@gmail.com>
2019-08-03 00:07:08 -0400

    pomodoro counts down now; still don't know what I'm doing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const Button = (props) => {
+  return (
+    <button onClick={props.handler}> 
+      {props.text}
+    </button>
+  );
 }
+
+const Display = (props) => {  
+  const interval = setTimeout(() => {
+    console.log('targetTime is ' + props.targetTime + ' seconds');
+
+    props.update(props.targetTime - 1);
+  }, 1000);
+
+
+  let remainingMinutes = Math.floor(props.targetTime / 60);
+  let remainingSeconds = props.targetTime % 60;
+  
+  // hacky, but whatevz
+  if (remainingMinutes === 0) {
+    remainingMinutes = '00';
+  }
+
+  if (remainingMinutes < 10) {
+    remainingMinutes = '0' + remainingMinutes;
+  }
+
+  if (remainingSeconds < 10) {
+    remainingSeconds = '0' + remainingSeconds;
+  }
+
+  if (remainingSeconds === 0) {
+    remainingSeconds = '00';
+  }
+
+  if (remainingMinutes == 0 && remainingSeconds == 0) {
+    clearInterval(interval);
+  }
+
+  return (
+    <div>{remainingMinutes}:{remainingSeconds} left</div>
+  );
+}
+
+
+const App = () => {
+    let targetTimeInSeconds = 25 //* 60; // 25 minutes * 60 seconds = 1500
+    //let targetTimeInSeconds = Math.round(24.233 * 60); // this is 24 minutes and 5 seconds 
+    const [countdownTime, updateCountdownTime] = React.useState(targetTimeInSeconds)
+    
+    //let isCountingDown = false;
+
+    let startTimer = () => {
+      //isCountingDown = true; 
+    };
+    
+    let stopTimer = () => {
+      
+      //isCountingDown = false;
+    };
+    
+    let resetTimer = () => {
+      updateCountdownTime(targetTimeInSeconds);
+    }; 
+  
+    return (
+      <div>
+        <Button text="Start" handler={startTimer} />
+        &nbsp;
+        <Button text="Stop" handler={stopTimer} />
+        &nbsp;
+        <Button text="Reset" handler={resetTimer} />
+        &nbsp;
+        <Display update={updateCountdownTime} targetTime={countdownTime}/>
+      </div>
+    ); 
+}
+
 
 export default App;
