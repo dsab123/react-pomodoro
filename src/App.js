bbbcabb89088602b973637216812941f4bc77b15
dsab123 <dsabbaghumd@gmail.com>
2019-08-05 13:24:46 -0400

    add  to handle timer countdown, make buttons work

--- src/App.js
+++ src/App.js
@@ -3,42 +3,50 @@ import './App.css';
 
 const Button = (props) => {
   return (
-    <button onClick={props.handler}> 
+    <button onClick={props.clickHandler}> 
       {props.text}
     </button>
   );
 }
 
-const Display = (props) => {  
-  const interval = setTimeout(() => {
-    console.log('targetTime is ' + props.targetTime + ' seconds');
+const Display = (props) => {
+  const [stopTicking, updateStopTicking] = React.useState(false);
+  
+  // If timer is not stopped or has finished, we continue to count down.
+  React.useEffect(() => {
+      if (!props.pauseTimer && !stopTicking) {
+      const timeout = setTimeout(() => {    
+        props.update(props.targetTime - 1);
+      }, 1000);
 
-    props.update(props.targetTime - 1);
-  }, 1000);
-
-
+      return () => clearTimeout(timeout);
+    }
+  });
+  
+  
   let remainingMinutes = Math.floor(props.targetTime / 60);
   let remainingSeconds = props.targetTime % 60;
   
-  // hacky, but whatevz
-  if (remainingMinutes === 0) {
-    remainingMinutes = '00';
-  }
-
   if (remainingMinutes < 10) {
     remainingMinutes = '0' + remainingMinutes;
-  }
+  } else if (remainingMinutes === 0) {
+    remainingMinutes = '00';
+  }  
 
   if (remainingSeconds < 10) {
     remainingSeconds = '0' + remainingSeconds;
-  }
-
-  if (remainingSeconds === 0) {
+  } else if (remainingSeconds === 0) {
     remainingSeconds = '00';
   }
 
-  if (remainingMinutes == 0 && remainingSeconds == 0) {
-    clearInterval(interval);
+  if (props.targetTime === 0) {
+    if (!stopTicking) {
+      updateStopTicking(true);
+    }
+  } else if (props.targetTime > 0) {
+    if (stopTicking) {
+      updateStopTicking(false);
+    }
   }
 
   return (
@@ -46,39 +54,35 @@ const Display = (props) => {
   );
 }
 
-
 const App = () => {
-    let targetTimeInSeconds = 25 //* 60; // 25 minutes * 60 seconds = 1500
-    //let targetTimeInSeconds = Math.round(24.233 * 60); // this is 24 minutes and 5 seconds 
-    const [countdownTime, updateCountdownTime] = React.useState(targetTimeInSeconds)
+  let targetTimeInSeconds = 25 * 60; // 25 minutes * 60 seconds = 1500 seconds 
+  const [countdownTime, updateCountdownTime] = React.useState(targetTimeInSeconds) 
+  const [pauseTimer, updatePauseTimer] = React.useState(false);
     
-    //let isCountingDown = false;
+  let startTimerHandler = () => {
+    updatePauseTimer(false);
+  };
+    
+  let pauseTimerHandler = () => {
+    updatePauseTimer(true);  
+  };
+    
+  let resetTimerHandler = () => {
+    updateCountdownTime(targetTimeInSeconds);
+    updatePauseTimer(true);
+  }; 
 
-    let startTimer = () => {
-      //isCountingDown = true; 
-    };
-    
-    let stopTimer = () => {
-      
-      //isCountingDown = false;
-    };
-    
-    let resetTimer = () => {
-      updateCountdownTime(targetTimeInSeconds);
-    }; 
-  
-    return (
-      <div>
-        <Button text="Start" handler={startTimer} />
-        &nbsp;
-        <Button text="Stop" handler={stopTimer} />
-        &nbsp;
-        <Button text="Reset" handler={resetTimer} />
-        &nbsp;
-        <Display update={updateCountdownTime} targetTime={countdownTime}/>
-      </div>
-    ); 
+  return (
+    <div>
+      <Button text="Start" clickHandler={startTimerHandler} />
+      &nbsp;
+      <Button text="Pause" clickHandler={pauseTimerHandler} />
+      &nbsp;
+      <Button text="Reset" clickHandler={resetTimerHandler} />
+      &nbsp;
+      <Display update={updateCountdownTime} pauseTimer={pauseTimer} targetTime={countdownTime}/>
+    </div>
+  ); 
 }
-
 
 export default App;
